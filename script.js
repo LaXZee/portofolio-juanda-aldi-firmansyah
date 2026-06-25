3148a212035a847de3db4e4dbc16d04ae801b9c6
LaXZee <juandaaldifirmansyah@gmail.com>
2026-06-25 20:44:49 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Navigasi Mobile (Hamburger Menu)
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
+
+// Tutup menu otomatis setelah klik tautan menu
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        mobileMenu.classList.remove('active');
+        navLinks.classList.remove('active');
+    });
+});
